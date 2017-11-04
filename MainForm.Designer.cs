7e04a50893fef37989e243e8de24cc72cf707b07
Manfred Kuhn <manfred.kuhn@de.trumpf.com>
2017-11-04 08:59:29 +0100

    Initialer Commit Version 0

---
+++ MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace KeepDisk
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+            System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+            System.Windows.Forms.Timer timer;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this._notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            timer = new System.Windows.Forms.Timer(this.components);
+            contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip
+            // 
+            contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            exitMenuItem});
+            contextMenuStrip.Name = "_contextMenuStrip";
+            contextMenuStrip.Size = new System.Drawing.Size(121, 26);
+            // 
+            // exitMenuItem
+            // 
+            exitMenuItem.Name = "exitMenuItem";
+            exitMenuItem.Size = new System.Drawing.Size(120, 22);
+            exitMenuItem.Text = "Be&enden";
+            exitMenuItem.Click += new System.EventHandler(this.OnExitApp);
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 5000;
+            timer.Tick += new System.EventHandler(this.OnCheckDisk);
+            // 
+            // _notifyIcon
+            // 
+            this._notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this._notifyIcon.BalloonTipText = "Prüfe alle 10 min.";
+            this._notifyIcon.BalloonTipTitle = "Keep Disk Alive";
+            this._notifyIcon.ContextMenuStrip = contextMenuStrip;
+            this._notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("_notifyIcon.Icon")));
+            this._notifyIcon.Text = "Keep Disk Alive";
+            this._notifyIcon.Visible = true;
+            this._notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ShowBallon);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keep Disk";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeepDisk
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var f = new FileInfo(Application.ExecutablePath);
+            _notifyIcon.BalloonTipText = $"Hallo {Environment.UserName}\nIch prüfe das Laufwerk {f.Directory.Root} alle 5 Sekunden.";
+        }
+
+        private void OnExitApp(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnCheckDisk(object sender, EventArgs e)
+        {
+            var f = new FileInfo(Application.ExecutablePath);
+            Console.WriteLine(f.Exists);
+        }
+
+        private void ShowBallon(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+                _notifyIcon.ShowBalloonTip(5000);
+        }
+    }
+}
